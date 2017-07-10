953f856a926313f0dbe0700c641c9ef82a840ac2
dmaisano <dev.dmaisano@gmail.com>
2017-07-10 09:27:38 -0400

    updated package.json & gulpfile.js

--- gulpfile.js
+++ gulpfile.js
@@ -1,4 +1,42 @@
-require('./gulp/tasks/styles'); // import styles.js
-require('./gulp/tasks/watch'); // import watch.js
+let gulp = require('gulp');
+let watch = require('gulp-watch');
+let browserSync = require('browser-sync').create();
+let sass = require('gulp-sass');
+let cleanCSS = require('gulp-clean-css');
+let rename = require("gulp-rename");
 
-//comment
+// Browser-Sync Task with functionality
+gulp.task('browser-sync', () => {
+  browserSync.init(["index.html", "assests/images/**/*.png", "./app/dist/styles/*.css"], {
+    notify: false, // disables browser-sync notifications in top-right corner
+    server: {
+      baseDir: "app"
+    }
+  });
+
+  //watch('./app/assets/styles/**/*.css', () => {
+  //  gulp.start('cssInject');
+  //});
+});
+
+// Compile styles.scss (SASS to CSS)
+gulp.task('sass', () => {
+  gulp.src('sass/main.scss') // only the main sass file is required as it imports the other sass modules
+    .pipe(sass({includePaths: ['scss']}))
+    .pipe(cleanCSS({compatibility: 'ie8'}))
+    .pipe(rename({ suffix: '.min' }))
+    .pipe(gulp.dest('./app/dist'));
+});
+
+// Task to Inject CSS without browser-sync doing a full refresh (f5)
+gulp.task('cssInject', ['sass'], () => { // gulp.task('styles') will run within this task
+  return gulp.src('./app/dist/styles.css')
+    .pipe(browserSync.stream());
+});
+
+
+// Default task that will run which handles the tasks above
+gulp.task('default', ['sass', 'cssInject', 'browser-sync'], () => {
+  gulp.watch('./app/assets/styles/**/*.css', ['sass']);
+  gulp.watch("./app/dist/styles.min.css", ['cssInject']);
+});
