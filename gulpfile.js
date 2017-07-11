088763851e3d009937de6dd127a1ce3bf34e10dc
dmaisano <dev.dmaisano@gmail.com>
2017-07-11 13:43:16 -0400

    changed sass styles to PostCSS and updated gulpfile.js

--- gulpfile.js
+++ gulpfile.js
@@ -1,7 +1,11 @@
 let gulp = require('gulp');
 let watch = require('gulp-watch');
 let browserSync = require('browser-sync').create();
-let sass = require('gulp-sass');
+let postCSS = require('gulp-postcss');
+let cssImport = require('postcss-import');
+let cssVars = require('postcss-simple-vars');
+let nested = require('postcss-nested');
+let autoprefixer = require('autoprefixer');
 let cleanCSS = require('gulp-clean-css');
 let rename = require("gulp-rename");
 
@@ -10,29 +14,33 @@ gulp.task('browser-sync', () => {
   browserSync.init(["index.html", "assests/images/**/*.png", "./app/dist/styles/*.css"], {
     notify: false, // disables browser-sync notifications in top-right corner
     server: {
-      baseDir: "app"
+      baseDir: "./"
     }
   });
 });
 
-// Compile styles.scss (SASS to CSS)
-gulp.task('sass', () => {
-  gulp.src('sass/main.scss') // only the main sass file is required as it imports the other sass modules
-    .pipe(sass({includePaths: ['scss']}))
+// Compile PostCSS to regular CSS to be used in the browser
+gulp.task('post-css', () => {
+  gulp.src('./app/assets/styles/styles.css')
+    .pipe(postCSS([cssImport, cssVars, nested, autoprefixer]))
     .pipe(cleanCSS({compatibility: 'ie8'}))
     .pipe(rename({ suffix: '.min' }))
+    .on('error', (errorLog) => {
+      console.log(errorLog.toString());
+      this.emit('end');
+    })
     .pipe(gulp.dest('./app/dist'));
 });
 
 // Task to Inject CSS without browser-sync doing a full refresh (f5)
-gulp.task('cssInject', ['sass'], () => { // gulp.task('sass') will run within this task
+gulp.task('cssInject', ['post-css'], () => { // gulp.task('post-css') will run within this task
   return gulp.src('./app/dist/styles.css')
     .pipe(browserSync.stream());
 });
 
 
 // Default task that will run which handles the tasks above
-gulp.task('default', ['sass', 'cssInject', 'browser-sync'], () => {
-  gulp.watch('./app/assets/styles/**/*.css', ['sass']);
+gulp.task('default', ['post-css', 'cssInject', 'browser-sync'], () => {
+  gulp.watch('./app/assets/styles/**/*.css', ['post-css']);
   gulp.watch("./app/dist/styles.min.css", ['cssInject']);
 });
