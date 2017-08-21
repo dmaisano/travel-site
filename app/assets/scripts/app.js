46cca81584f87ef4b83ea61a22980a8462696b3e
dmaisano <dev.dmaisano@gmail.com>
2017-08-20 22:56:52 -0400

    wip sticky header

--- app/assets/scripts/app.js
+++ app/assets/scripts/app.js
@@ -1,7 +1,9 @@
 import MobileMenu from './modules/MobileMenu';
 import LazyLoad from './modules/LazyLoad';
+import StickyHeader from './modules/StickyHeader';
 import $ from 'jquery';
 
 let menu = new MobileMenu();
 let featureItems = new LazyLoad($(".feature-item"), "85%");
-let testimonial = new LazyLoad($(".testimonial"), "60%");
+let testimonial = new LazyLoad($(".testimonial"), "70%");
+let stickyHeader = new StickyHeader();
