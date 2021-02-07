5bdd49e7f9ba6eefcaffb65de635ec66f97aaa22
Domenico Maisano <dmaisanooo@gmail.com>
2021-02-07 10:46:49 -0500

    Clean project

--- app/assets/scripts/app.js
+++
@@ -1,9 +0,0 @@
-import MobileMenu from './modules/MobileMenu';
-import LazyLoad from './modules/LazyLoad';
-import StickyHeader from './modules/StickyHeader';
-import $ from 'jquery';
-
-let menu = new MobileMenu();
-let featureItems = new LazyLoad($(".feature-item"), "85%");
-let testimonial = new LazyLoad($(".testimonial"), "70%");
-let stickyHeader = new StickyHeader();
